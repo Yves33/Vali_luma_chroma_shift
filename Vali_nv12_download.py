699b82037593f234a420a88bbf6fcdcf5a556b92
Yves33 <yves.le-feuvre@u-bordeaux.fr>
2024-07-23 08:13:43 +0200

    understood bug!

--- Vali_nv12_download.py
+++ Vali_nv12_download.py
@@ -1,15 +1,7 @@
 from PIL import Image,ImageShow
 import numpy as np
 import PyNvCodec as nvc
-import os,sys
-
-try:
-    import pycuda
-    import pycuda.autoinit
-    CUDAAVAIL=True
-except:
-    CUDAAVAIL=False
-    print("Pycuda is required to run this test")
+import os,sys,time
 
 def make_img(buffer,w,h,format,merge=False):
     """
@@ -86,45 +78,31 @@ def decode_vali(srcfile,nframes=20):
     rgbBuffer = np.zeros((nvDec.Width(),nvDec.Height(),3),np.uint8)
     nv12Buffer= np.zeros((nvDec.Width()*(3*nvDec.Height())//2),np.uint8)
     cc_ctx=None
-    print(f"#### {nvDec.Format()} {nvDec.ColorSpace()} {nvDec.ColorRange()} ####")
-    if 1: #cSpace==nvc.ColorSpace.BT_709:
-        nvCvt=TwoPassConverter(nvDec.Format(),nvDec.Width(),nvDec.Height())
-    else:# cSpace==nvc.ColorSpace.BT_601:
-        nvCvt=OnePassConverter(nvDec.Format(),nvDec.Width(),nvDec.Height())
+    nvCvt=OnePassConverter(nvDec.Format(),nvDec.Width(),nvDec.Height())
 
     nvDwn = nvc.PySurfaceDownloader(gpu_id=0)
-    nvCvt=TwoPassConverter(nvDec.Format(),nvDec.Width(),nvDec.Height())
     for idx in range(nframes):
         rawSurface=nvc.Surface.Make(nvDec.Format(), nvDec.Width(),nvDec.Height(), 0)
         rgbSurface = nvc.Surface.Make(nvc.PixelFormat.RGB, nvDec.Width(),nvDec.Height(), 0)
         success, info = nvDec.DecodeSingleSurface(rawSurface)
         check(success,info)
+        #time.sleep(0.001)
         if 1:
             ## download nv12 plane, convert with PIL and save
             success = nvDwn.Run(rawSurface, nv12Buffer)
             check(success,info)
-            make_img(nv12Buffer,nvDec.Width(),nvDec.Height(),'NV12',merge=True).save(f"./{basename}/PIL_{idx+1:02}.jpg")
-        if 0 and CUDAAVAIL:
-            ## same, but download using pycuda (requires GpuMem())!
-            ## works correcttly!
-            device_to_host=pycuda.driver.Memcpy2D()
-            device_to_host.set_src_device(rawSurface.PlanePtr(0).GpuMem())
-            device_to_host.set_dst_host(nv12Buffer)
-            device_to_host.width_in_bytes = rawSurface.PlanePtr(0).Width()
-            device_to_host.src_pitch = rawSurface.PlanePtr(0).Pitch()
-            device_to_host.dst_pitch = rawSurface.PlanePtr(0).Width()
-            device_to_host.src_height = rawSurface.PlanePtr(0).Height()
-            device_to_host.height = rawSurface.PlanePtr(0).Height()
-            device_to_host(aligned=True)
-            make_img(nv12Buffer,nvDec.Width(),nvDec.Height(),'NV12',merge=True).save(f"./{basename}/PIL_{idx+1:02}.jpg")
-
+            make_img(nv12Buffer,nvDec.Width(),nvDec.Height(),'NV12',merge=True)\
+                .resize((640,320))\
+                .save(f"./{basename}/PIL_{idx+1:02}.jpg")
         if 1:
             ## convert to rgb, download and display
             success, info=nvCvt.Run(rawSurface,rgbSurface,cc_ctx)
             check(success,info)
             success = nvDwn.Run(rgbSurface, rgbBuffer)
             check(success,info)
-            make_img(rgbBuffer,nvDec.Width(),nvDec.Height(),'RGB',merge=True).save(f"./{basename}/VALI_{idx+1:02}.jpg")
+            make_img(rgbBuffer,nvDec.Width(),nvDec.Height(),'RGB',merge=True)\
+                .resize((640,320))\
+                .save(f"./{basename}/VALI_{idx+1:02}.jpg")
 
 if __name__=='__main__':
     nframes=20
